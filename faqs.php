e7df7e2258d3910c4515b2bc043fbbbb0d4bb0d7
alisonwen123 <Son#son2>
2015-04-14 17:03:23 -0500

    the FAQs page is now in php

---
+++ faqs.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Frequently Asked Questions</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">GameOfStocks</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+            <li><a href="./index.html">Game</a></li>
+            <li><a href="./about.html">About Us</a></li>
+            <li class="active"><a href="./faqs.html">Contact/FAQS</a></li>
+         </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+	<br></br>
+
+    <div class="container">
+      <!-- Example row of columns -->
+		<h3><a href = "#stocks">Stocks</a></h3>
+		<ol>
+			<li>What are stocks?</li>
+			<li>What is a stockholder?</li>
+			<li>How do stocks work?</li>
+			<li>Types of Stocks</li>
+		</ol>
+		<h3><a href="#setup">Set Up</a></h3>
+		<ol>
+			<li>blah blah</li>
+			<li>blah blah</li>
+			<li>blah blah</li>
+		</ol>
+        <h3><a href="#howto">How to Play</a></h3>
+        <ol>
+			<li>blah blah</li>
+			<li>blah blah</li>
+			<li>blah blah</li>
+		</ol>
+        <h3><a href="#Q4">Q4</a></h3>
+        <ol>
+			<li>blah blah</li>
+			<li>blah blah</li>
+			<li>blah blah</li>
+		</ol>
+		
+		<hr>
+		<h2><a name = "stocks"></a>Stocks</h2>
+		<strong>1. What are stocks? </strong>
+		<p>Stocks are one of the most popular financial instruments in the world. It is the share of the ownership of the company.</p>
+		<strong>2. What is a stockholder? </strong>
+		<p>The stockholder is the owner of a stock</p>
+		<strong>3. How do stocks work?</strong>
+		<p>Most stocks are traded on exchanges, places where buyers and sellers meet and decide on a price. Some exchanges are physical locations where transactions are carried out on a trading floor. The other type of exchange is virtual, trades are made electronically</p>
+		<strong>4. Types of Stocks</strong>
+		<p><strong>Common Stock</strong></p>
+		<p>When people talk about stocks they are usually referring to common stock. The majority of stock is issued in this form. Common shares represent ownership in a company and a claim (dividends) on a portion of profits.</p>
+		<p><strong>Preferred Stock</strong></p>
+		<p>Preferred stock represents some degree of ownership in a company but usually doesn't come with the same voting rights. With this type of stock, investors re usually guaranteed a fixed dividend forever while common stock has variable dividends that are never guaranteed. Preferred shareholders are paid off before the common shareholder in case of liquidation. Preferred stocks may also be callable (the company has the option to purchase the shares from shareholders at any time for any reason).</p>
+		<a href="#top">To the top</a>
+		<hr>
+		<h2><a name="setup"></a>Set Up</h2>
+		<a href="#top">To the top</a>
+		<hr>
+		<h2><a name = "howto"></a>How to Play</h2>
+		<a href="#top">To the top</a>
+		<hr>
+		<h3><a name = Q4></a>Q4</h3>
+		<a href="#top">To the top</a>
+		<hr>
+		
+		<p> If you still have questions, <a href="./contact.html">Contact Us</a/>. <a href="#top">To the top</a></p>
+      <footer>
+        <p>&copy; Lame Thing Here 2015</p>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
