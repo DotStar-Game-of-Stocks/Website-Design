cd2f95d1c7d63ceba270602501d355a982899d7c
alisonwen123 <Son#son2>
2015-04-14 17:03:38 -0500

    The portoflio wireframe is now in php

---
+++ portfolio_wireframe.php
@@ -0,0 +1,154 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio Wireframe</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+<body>
+   <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="./index.html">Stock Forecast</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+            <li><a href="./index.html">Game</a></li>
+            <li class="active"><a href="./about.html">About Us</a></li>
+            <li><a href="./faqs.html">Contact/FAQS</a></li>
+			<li>
+				<form class="navbar-form navbar-left" role="search">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+			</li>
+         </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+	<br></br>
+    <!-- NAVBAR CODE END -->
+
+
+    <div class="container">
+        <section style="padding-bottom: 50px; padding-top: 50px;">
+			<h2>Portfolio Name</h2>
+			<h4>Portfolio Value</h4>
+			<h4>$100,000,000.00</h4>
+			<h4><font color="green">00.00%</font><font color="red">00.00%</font></h4>
+			<p></p>
+			<p></p>
+			<h4><strong>Available Cash</strong></h4>
+			<h4>$90,000,000.00</h4>
+			<button type="submit" class="btn btn-default">Make new trade</button> 
+			<h4>Stocks owned</h4>
+			<table class="table table-stribed table-content table-condensed table-striped">
+				<tr>
+					<td><strong>Symbol</strong></td>
+					<td><strong>Shares</strong></td>
+					<td><strong>Purchase Price</strong></td>
+					<td><strong>Market Price</strong></td>
+					<td><strong>Gain/Loss</strong></td>
+					<td><strong>Gain/Loss %</strong></td>
+					<td><strong>Actions</strong></td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+					<td>$0.00</td>
+					<td><font color="green">$0.00</font><font color="red">$0.00</font></td>
+					<td><font color="green">00.00%</font><font color="red">00.00%</font></td>
+					<td>Buy or Sell</td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+					<td>$0.00</td>
+					<td><font color="green">$0.00</font><font color="red">$0.00</font></td>
+					<td><font color="green">00.00%</font><font color="red">00.00%</font></td>
+					<td>Buy or Sell</td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+					<td>$0.00</td>
+					<td><font color="green">$0.00</font><font color="red">$0.00</font></td>
+					<td><font color="green">00.00%</font><font color="red">00.00%</font></td>
+					<td>Buy or Sell</td>
+				</tr>
+				<tr>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+					<td>$0.00</td>
+					<td><font color="green">$0.00</font><font color="red">$0.00</font></td>
+					<td><font color="green">00.00%</font><font color="red">00.00%</font></td>
+					<td>Buy or Sell</td>
+				</tr>
+			</table>
+			
+			<h4>Trade History</h4>
+			<table class="table table-stribed table-content table-condensed table-striped">
+				<tr>
+					<td><strong>Buy or Sell</strong></td>
+					<td><strong>Symbol</strong></td>
+					<td><strong>Shares</strong></td>
+					<td><strong>Price</strong></td>
+				</tr>
+				<tr>
+					<td>Buy</td>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+				</tr>
+				<tr>
+					<td>Sell</td>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+				</tr>
+				<tr>
+					<td>Buy</td>
+					<td>Name</td>
+					<td>0</td>
+					<td>$0.00</td>
+				</tr>
+			</table>
+			
+        </section>
+        <!-- SECTION END -->
+    </div>
+    <!-- CONATINER END -->
+
+    <!-- REQUIRED SCRIPTS FILES -->
+    <!-- CORE JQUERY FILE -->
+	    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
