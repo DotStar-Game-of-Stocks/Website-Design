45763aaae8052d5263d0f38d46ef27e921236e09
alisonwen123 <Son#son2>
2015-04-21 20:23:24 -0500

    mail.php that takes contact data

---
+++ mail.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['InputName'];
+$email = $_POST['InputEmail'];
+$message = $_POST['InputMessage'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "root@stockforecast.ga";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
