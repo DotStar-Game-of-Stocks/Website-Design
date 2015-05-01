132875eee903c4e11bb03a0903841ccddd376601
alisonwen123 <Son#son2>
2015-05-01 16:09:43 -0500

    what I have so far

--- login.php
+++ login.php
@@ -1,15 +1,5 @@
 <?php
-	$dbhost = 'localhost';
-	$dbuser = 'root';
-	$dbpass = 'stockforcast';
-	$database = 'forecast';
-	$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-	if(! $conn )
-	{
-	die('Could not connect: ' . mysql_error());
-	}
-	mysql_select_db($database, $conn) or die("Oops some thing went wrong");
-	//include("config.php");
+	include("config.php");
 	//session starts
 	session_start();
 	if($_SERVER["REQUEST_METHOD"] == "POST")
